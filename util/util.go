fd9444a5317b31e90eaaf6a3b61691a94eec2a57
Patrik Nordling <nils.patrik@gmail.com>
2017-02-07 22:11:50 +0100

    adding util

---
+++ util/util.go
@@ -0,0 +1,19 @@
+package util
+
+import "github.com/influxdata/influxdb/client"
+
+func QueryDB(clnt client.Client, cmd string, MyDB string) (res []client.Result, err error) {
+	q := client.Query{
+		Command:  cmd,
+		Database: MyDB,
+	}
+	if response, err := clnt.Query(q); err == nil {
+		if response.Error() != nil {
+			return res, response.Error()
+		}
+		res = response.Results
+	} else {
+		return res, err
+	}
+	return res, nil
+}
