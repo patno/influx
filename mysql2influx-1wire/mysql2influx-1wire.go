1aee568b3e1cf1899d4291f509393cb559d9f53b
Patrik Nordling <nils.patrik@gmail.com>
2017-02-17 23:02:08 +0100

    bug fix table name

--- mysql2influx-1wire/mysql2influx-1wire.go
+++ mysql2influx-1wire/mysql2influx-1wire.go
@@ -27,7 +27,7 @@ func main() {
 	log.Printf("Latest Influx DB timestamp:%v\n", latestTimestamp)
 
 	mysqlDbQuery := fmt.Sprintf(
-		"select timestamp, deviceid, value from 1wire.network where timestamp > '%v' order by timestamp", latestTimestamp)
+		"select timestamp, deviceid, value from 1wire.temperature where timestamp > '%v' order by timestamp", latestTimestamp)
 
 	log.Printf("MySQL Query:%v\n", mysqlDbQuery)
 	rows, _, err := db.Query(mysqlDbQuery)
