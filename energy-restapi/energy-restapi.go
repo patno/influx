cd80b71f9e1f0ef4f27162335ce3b7900a4cebf9
Patrik Nordling <nils.patrik@gmail.com>
2017-03-11 10:01:56 +0100

    cleaning

--- energy-restapi/energy-restapi.go
+++ energy-restapi/energy-restapi.go
@@ -10,20 +10,6 @@ import (
 	"github.com/patno/influx/util"
 )
 
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
-
 type QueryResponseAPI struct {
 	Target     string     `json:"target,omitempty"`
 	Datapoints [][2]int64 `json:"datapoints",omitempty`
@@ -45,41 +31,6 @@ type QueryRequestAPI struct {
 	} `json:"rangeRaw"`
 	Interval   string `json:"interval"`
 	IntervalMs int    `json:"intervalMs"`
-}
-
-func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Person{})
-}
-
-func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	var person Person
-	_ = json.NewDecoder(req.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
-}
-
-func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(people)
 }
 
 func setCORSHeaders(w http.ResponseWriter) {
@@ -156,20 +107,15 @@ func getQueryAPI(w http.ResponseWriter, request *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "Nic", Lastname: "Raboy", Address: &Address{City: "Dublin", State: "CA"}})
-	people = append(people, Person{ID: "2", Firstname: "Maria", Lastname: "Raboy"})
-	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-	log.Println("energy rest API -------------------")
+	port := ":8345"
+	log.Printf("energy rest API %v-------------------\n", port)
 	router.HandleFunc("/", getTestConnectionAPI)
 	router.HandleFunc("/", getTestConnectionAPI)
 	router.HandleFunc("/search", getSearchAPI)
 	router.HandleFunc("/query", getQueryAPI)
 	router.HandleFunc("/annotations", postAnnotationsAPI)
 
-	err := http.ListenAndServe(":8345", router)
+	err := http.ListenAndServe(port, router)
 	if err != nil {
 		log.Fatal(err)
 	}
